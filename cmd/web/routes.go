7234858a53338852b1d8a2ed53b3c0d8260294aa
jakubmajer@live.com <jakubmajer@live.com>
2024-04-13 18:26:26 +0200

    first commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Timeout(60 * time.Second))
+
+	return mux
+}
